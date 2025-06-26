1dd7639e9e4e385d68cd0d4a9a6c248b166b1041
Alex Eagle <alex@aspect.build>
2025-06-26 12:46:30 -0600

    fix: make our BCR release green (#5)

---
+++ .github/workflows/release_prep.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+# Argument provided by reusable workflow caller, see
+# https://github.com/bazel-contrib/.github/blob/d197a6427c5435ac22e56e33340dff912bc9334e/.github/workflows/release_ruleset.yaml#L72
+TAG=$1
+# The prefix is chosen to match what GitHub generates for source archives
+# This guarantees that users can easily switch from a released artifact to a source archive
+# with minimal differences in their code (e.g. strip_prefix remains the same)
+PREFIX="bazelrc-preset.bzl-${TAG:1}"
+ARCHIVE="bazelrc-preset.bzl-$TAG.tar.gz"
+
+# NB: configuration for 'git archive' is in /.gitattributes
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
+
+cat << EOF
+Add to your \`MODULE.bazel\` file:
+
+\`\`\`starlark
+bazel_dep(name = "bazelrc-preset.bzl", version = "${TAG:1}")
+\`\`\`
+
+Then follow more install instructions from the project README.
+EOF
